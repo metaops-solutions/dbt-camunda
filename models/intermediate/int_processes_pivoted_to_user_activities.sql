c866bf14b884c4f06d46967e996d7baf1200cf4e
Jim Collins <jim.collins@digital.homeoffice.gov.uk>
2024-01-09 17:36:55 +0000

    Renamed views

--- models/intermediate/int_processes_pivoted_to_user_activities.sql
+++ models/intermediate/int_processes_pivoted_to_user_activities.sql
@@ -14,10 +14,6 @@ pivot_and_split_processes_to_user_activities_grain as (
 select pi.business_key, ai.activity_name ,ai.start_time, ai.end_time, ai.duration, ai.sequence_counter
 from processes pi, activities ai
 where pi.root_process_instance_id = ai.root_process_instance_id
-<<<<<<< HEAD
-  and ai.activity_type = 'userTask'
-=======
->>>>>>> 8522fd1 (Added additional views)
   and ai.sequence_counter =
       (select max(ai2.sequence_counter) from activities ai2 where
           ai.root_process_instance_id = ai2.root_process_instance_id)
